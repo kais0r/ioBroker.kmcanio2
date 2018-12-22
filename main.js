0c9b0c30a703bc1ab231ce379a3f39644dc7558c
M.Eng. Franz Kaiser <franz.kaiser@kaiser-mechatronik.de>
2018-12-23 00:57:05 +0100

    Created local parallel vars. Upadte iobroker Objects only on change. Outputs now controllable. Still early test stage

--- main.js
+++ main.js
@@ -54,6 +54,24 @@ var txPDO1 = new Buffer([0,0,0,0,0,0,0,0]);
 var KnownDevices = [];
 
 
+// CAM CANIO Devices
+var km_canio_buffers = [];
+km_canio_buffers[0x01] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x02] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x03] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x04] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x05] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x06] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x07] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x08] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x09] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0A] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0B] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0C] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0D] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0E] = new Buffer([0,0,0,0,0,0,0,0]);
+km_canio_buffers[0x0F] = new Buffer([0,0,0,0,0,0,0,0]);
+
 
 /*Variable declaration, since ES6 there are let to declare variables. Let has a more clearer definition where 
 it is available then var.The variable is available inside a block and it's childs, but not outside. 
@@ -85,6 +103,9 @@ adapter.on('objectChange', function (id, obj) {
 
 // is called if a subscribed state changes
 adapter.on('stateChange', function (id, state) {
+
+	adapter.log.info('---- stateChange ----');
+
     // Warning, state can be null if it was deleted
     //adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 	
@@ -107,54 +128,65 @@ adapter.on('stateChange', function (id, state) {
 		if( split_array[1]=="RelayOutput"){
 			if( split_array[0].includes("KMsmartIO_") ){
 				
+				var DeviceName = split_array[0];
+				
 				var NodeID =  parseInt( split_array[0].replace("KMsmartIO_", "") );
 				
-				var RelayNumber = parseInt( split_array[2].replace("K", "") ) - 1;
-				var RelayByte = RelayNumber>>3;	// = RelayNumber/8
-				var RelayBit  = RelayNumber%8;
+				var RelayNumber = parseInt( split_array[2].replace("K", "") );
+				var RelayByte = (RelayNumber-1)>>3;
+				var RelayBit  = (RelayNumber-1)%8;
 				
 				adapter.log.info('NodeID ' + NodeID );
 				adapter.log.info('RelayNumber ' + RelayNumber );
 				adapter.log.info('RelayByte ' + RelayByte );
 				adapter.log.info('RelayBit ' + RelayBit );
 				
+				
+				if( typeof KnownDevices[DeviceName] === 'undefined' ){
+					adapter.log.info('ERROR, DeviceVar not found!' );
+					return;
+				}else{
+					
+					adapter.log.info('Device: ' + DeviceName + ', K Nr. ' + RelayNumber );
+					
+					
+					if( typeof KnownDevices[DeviceName]["K"+RelayNumber] === 'undefined' ){
+						adapter.log.info('ERROR, DeviceVar Relay not found!' );
+						return;
+					}else{
+						
+						KnownDevices[DeviceName]["K"+RelayNumber] = state.val;
+						
+						var txMSG = new Buffer([0,0,0,0,0,0,0,0]);
+						if( KnownDevices[DeviceName]["K1"]  == true ){ txMSG[0] |= 0x01; }
+						if( KnownDevices[DeviceName]["K2"]  == true ){ txMSG[0] |= 0x02; }
+						if( KnownDevices[DeviceName]["K3"]  == true ){ txMSG[0] |= 0x04; }
+						if( KnownDevices[DeviceName]["K4"]  == true ){ txMSG[0] |= 0x08; }
+						if( KnownDevices[DeviceName]["K5"]  == true ){ txMSG[0] |= 0x10; }
+						if( KnownDevices[DeviceName]["K6"]  == true ){ txMSG[0] |= 0x20; }
+						if( KnownDevices[DeviceName]["K7"]  == true ){ txMSG[0] |= 0x40; }
+						if( KnownDevices[DeviceName]["K8"]  == true ){ txMSG[0] |= 0x80; }
+						if( KnownDevices[DeviceName]["K9"]  == true ){ txMSG[1] |= 0x01; }
+						if( KnownDevices[DeviceName]["K10"] == true ){ txMSG[1] |= 0x02; }
+						if( KnownDevices[DeviceName]["K11"] == true ){ txMSG[1] |= 0x04; }
+						if( KnownDevices[DeviceName]["K12"] == true ){ txMSG[1] |= 0x08; }
+						if( KnownDevices[DeviceName]["K13"] == true ){ txMSG[1] |= 0x10; }
+						if( KnownDevices[DeviceName]["K14"] == true ){ txMSG[1] |= 0x20; }
+						if( KnownDevices[DeviceName]["K15"] == true ){ txMSG[1] |= 0x40; }
+						if( KnownDevices[DeviceName]["K16"] == true ){ txMSG[1] |= 0x80; }
+						if( KnownDevices[DeviceName]["K17"] == true ){ txMSG[2] |= 0x01; }
+						if( KnownDevices[DeviceName]["K18"] == true ){ txMSG[2] |= 0x02; }
+						
+						var txid = NodeID + 0x200;
+						bus.send({ id: txid, ext: false, data: txMSG });
+						
+					}
+				}
 			}
 		}
 	}
 	
 	
-	
-	
-	
-	/*
-	
-	if( NodeType == "io" ){
-	
-		if( id == (adapter.namespace + '.RelayOutput.K01') ){ if( state.val==true ){ txPDO1[0] |= 0x01; }else{ txPDO1[0] &= ~0x01; } }
-		if( id == (adapter.namespace + '.RelayOutput.K02') ){ if( state.val==true ){ txPDO1[0] |= 0x02; }else{ txPDO1[0] &= ~0x02; } }
-		if( id == (adapter.namespace + '.RelayOutput.K03') ){ if( state.val==true ){ txPDO1[0] |= 0x04; }else{ txPDO1[0] &= ~0x04; } }
-		if( id == (adapter.namespace + '.RelayOutput.K04') ){ if( state.val==true ){ txPDO1[0] |= 0x08; }else{ txPDO1[0] &= ~0x08; } }
-		if( id == (adapter.namespace + '.RelayOutput.K05') ){ if( state.val==true ){ txPDO1[0] |= 0x10; }else{ txPDO1[0] &= ~0x10; } }
-		if( id == (adapter.namespace + '.RelayOutput.K06') ){ if( state.val==true ){ txPDO1[0] |= 0x20; }else{ txPDO1[0] &= ~0x20; } }
-		if( id == (adapter.namespace + '.RelayOutput.K07') ){ if( state.val==true ){ txPDO1[0] |= 0x40; }else{ txPDO1[0] &= ~0x40; } }
-		if( id == (adapter.namespace + '.RelayOutput.K08') ){ if( state.val==true ){ txPDO1[0] |= 0x80; }else{ txPDO1[0] &= ~0x80; } }
-		if( id == (adapter.namespace + '.RelayOutput.K09') ){ if( state.val==true ){ txPDO1[1] |= 0x01; }else{ txPDO1[1] &= ~0x01; } }
-		if( id == (adapter.namespace + '.RelayOutput.K10') ){ if( state.val==true ){ txPDO1[1] |= 0x02; }else{ txPDO1[1] &= ~0x02; } }
-		if( id == (adapter.namespace + '.RelayOutput.K11') ){ if( state.val==true ){ txPDO1[1] |= 0x04; }else{ txPDO1[1] &= ~0x04; } }
-		if( id == (adapter.namespace + '.RelayOutput.K12') ){ if( state.val==true ){ txPDO1[1] |= 0x08; }else{ txPDO1[1] &= ~0x08; } }
-		if( id == (adapter.namespace + '.RelayOutput.K13') ){ if( state.val==true ){ txPDO1[1] |= 0x10; }else{ txPDO1[1] &= ~0x10; } }
-		if( id == (adapter.namespace + '.RelayOutput.K14') ){ if( state.val==true ){ txPDO1[1] |= 0x20; }else{ txPDO1[1] &= ~0x20; } }
-		if( id == (adapter.namespace + '.RelayOutput.K15') ){ if( state.val==true ){ txPDO1[1] |= 0x40; }else{ txPDO1[1] &= ~0x40; } }
-		if( id == (adapter.namespace + '.RelayOutput.K16') ){ if( state.val==true ){ txPDO1[1] |= 0x80; }else{ txPDO1[1] &= ~0x80; } }
-		if( id == (adapter.namespace + '.RelayOutput.K17') ){ if( state.val==true ){ txPDO1[2] |= 0x01; }else{ txPDO1[2] &= ~0x01; } }
-		if( id == (adapter.namespace + '.RelayOutput.K18') ){ if( state.val==true ){ txPDO1[2] |= 0x02; }else{ txPDO1[2] &= ~0x02; } }
-		
-		var txid = NodeID + 512;
-		
-		bus.send({ id: txid, ext: false, data: txPDO1 });
-	
-	}
-	*/
 	
     // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
@@ -195,9 +227,20 @@ function SetAndUpdateState( obj_id, obj_name, obj_value ){
 		native: {} 
 	});
 	
+		
 	adapter.setState(obj_id, {val: obj_value, ack: true});
 	
 }
+
+
+function UpdateObjectOnChange( DeviceName, VarName, obj_id, new_state ){
+	if( KnownDevices[DeviceName][VarName] != new_state ){
+		KnownDevices[DeviceName][VarName] = new_state;
+		adapter.setState(obj_id, {val: new_state, ack: true});
+	}
+}
+
+
 
 
 function main() {
@@ -217,40 +260,169 @@ function main() {
 	
 	
 
-	
+	// CAN RECEIVE FUNCTION
 	bus.addListener("onMessage", 
 		function(msg) { 
 			
 			if( msg.id > 0x180 && msg.id < 0x280 ){
 				
 				var NodeID = msg.id - 0x180;
-				
 				var NodeType = "";
 				var DeviceName = "";
 				
 				if( NodeID < 0x10 ){
 					NodeType = "io";
 					DeviceName = "KMsmartIO_" + NodeID;
+				}else{
+					NodeType = "in";
+					DeviceName = "KMsmartIN_" + NodeID;
+				}
+				
+				
+				// Make New?
+				if( typeof KnownDevices[DeviceName] === 'undefined' ){
 					
-					if( typeof KnownDevices[DeviceName] === 'undefined' ){
+					// Create Device Specific Vars
+					var entry = [];
+					
+					// Create a object for Device Name
+					adapter.setObjectNotExists(DeviceName, { 
+						type: 'state', 
+						common: { 
+							name: "KMsmartHomeDevice", 
+							type: 'number'
+						}, 
+						native: {} 
+					});
+					
+					// Receive Counter
+					entry["RXcnt"]  = 0;
+					adapter.setObjectNotExists(DeviceName+".RXcnt", { type: 'state', common: { name: 'Receive Counter' , type: 'number' }, native: {} });
+					
+					
+					if( NodeType == "io" ){
 						
-						KnownDevices[DeviceName] = NodeID;
+						// Create Digital Input Vars
+						for( var i=0 ; i<=7 ; i++ ){
+							// Local Copy
+							entry["DI1."+i]  = false;
+							entry["DI2."+i]  = false;
+							entry["DI3."+i]  = false;
+							
+							// ioBroker Objects
+							adapter.setObjectNotExists(DeviceName+".DI1."+i, { type: 'state', common: { name: 'Digital Input 1.'+i , type: 'boolean' }, native: {} });
+							adapter.setObjectNotExists(DeviceName+".DI2."+i, { type: 'state', common: { name: 'Digital Input 2.'+i , type: 'boolean' }, native: {} });
+							adapter.setObjectNotExists(DeviceName+".DI3."+i, { type: 'state', common: { name: 'Digital Input 3.'+i , type: 'boolean' }, native: {} });
+						}
 						
+						
+						// Create Relay Output Vars
 						for( var i=1 ; i<=18 ; i++ ){
+						
+							// Local Copy
+							entry["K"+i]  = false;
+							
+							// ioBroker Objects
 							var obj_name = DeviceName+'.RelayOutput.K';
 							if( i<10 ){ obj_name += '0'; }
 							obj_name += i;
 							adapter.setObjectNotExists(obj_name, { type: 'state', common: { name: 'Relay Output K'+i , type: 'boolean' }, native: {} });
 							adapter.subscribeStates(obj_name);
+							
 						}
+						
 						
 					}
 					
 					
-				}else{
-					NodeType = "in";
-					DeviceName = "KMsmartIN_" + NodeID;
+					if( NodeType == "in" ){
+						
+						// Create Digital Input Vars
+						for( var i=0 ; i<=7 ; i++ ){
+							// Local Copy
+							entry["DI1."+i]  = false;
+							entry["DI2."+i]  = false;
+							entry["DI3."+i]  = false;
+							if( i<=3 ){ entry["DI4."+i]  = false; }
+							
+							// ioBroker Objects
+							adapter.setObjectNotExists(DeviceName+".DI1."+i, { type: 'state', common: { name: 'Digital Input 1.'+i , type: 'boolean' }, native: {} });
+							adapter.setObjectNotExists(DeviceName+".DI2."+i, { type: 'state', common: { name: 'Digital Input 2.'+i , type: 'boolean' }, native: {} });
+							adapter.setObjectNotExists(DeviceName+".DI3."+i, { type: 'state', common: { name: 'Digital Input 3.'+i , type: 'boolean' }, native: {} });
+							if( i<=3 ){ adapter.setObjectNotExists(DeviceName+".DI4."+i, { type: 'state', common: { name: 'Digital Input 4.'+i , type: 'boolean' }, native: {} }); }
+						}
+						
+						// Temperatur Sensors
+						adapter.setObjectNotExists(DeviceName+".OneWireBus.T1", { 
+							type: 'state', 
+							common: { 
+								name: "OneWire Tempsensor 1 [°C]", 
+								type: 'number'
+							}, 
+							native: {} 
+						});
+						entry["T1"]  = 0;
+						
+						
+					}
+					
+					
+					// Add the local Copy
+					KnownDevices[DeviceName] = entry;
+					
 				}
+				
+				
+				// RECEIVE
+				KnownDevices[DeviceName]["RXcnt"]++;
+				adapter.setState(DeviceName+".RXcnt", {val: KnownDevices[DeviceName]["RXcnt"], ack: true});
+				
+				
+				if( NodeType == "in" || NodeType == "io" ){
+					UpdateObjectOnChange( DeviceName, "DI1.0", DeviceName+".DI1.0", (msg.data[0]&0x01)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.1", DeviceName+".DI1.1", (msg.data[0]&0x02)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.2", DeviceName+".DI1.2", (msg.data[0]&0x04)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.3", DeviceName+".DI1.3", (msg.data[0]&0x08)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.4", DeviceName+".DI1.4", (msg.data[0]&0x10)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.5", DeviceName+".DI1.5", (msg.data[0]&0x20)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.6", DeviceName+".DI1.6", (msg.data[0]&0x40)>0 );
+					UpdateObjectOnChange( DeviceName, "DI1.7", DeviceName+".DI1.7", (msg.data[0]&0x80)>0 );
+					
+					UpdateObjectOnChange( DeviceName, "DI2.0", DeviceName+".DI2.0", (msg.data[1]&0x01)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.1", DeviceName+".DI2.1", (msg.data[1]&0x02)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.2", DeviceName+".DI2.2", (msg.data[1]&0x04)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.3", DeviceName+".DI2.3", (msg.data[1]&0x08)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.4", DeviceName+".DI2.4", (msg.data[1]&0x10)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.5", DeviceName+".DI2.5", (msg.data[1]&0x20)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.6", DeviceName+".DI2.6", (msg.data[1]&0x40)>0 );
+					UpdateObjectOnChange( DeviceName, "DI2.7", DeviceName+".DI2.7", (msg.data[1]&0x80)>0 );
+					
+					UpdateObjectOnChange( DeviceName, "DI3.0", DeviceName+".DI3.0", (msg.data[2]&0x01)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.1", DeviceName+".DI3.1", (msg.data[2]&0x02)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.2", DeviceName+".DI3.2", (msg.data[2]&0x04)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.3", DeviceName+".DI3.3", (msg.data[2]&0x08)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.4", DeviceName+".DI3.4", (msg.data[2]&0x10)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.5", DeviceName+".DI3.5", (msg.data[2]&0x20)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.6", DeviceName+".DI3.6", (msg.data[2]&0x40)>0 );
+					UpdateObjectOnChange( DeviceName, "DI3.7", DeviceName+".DI3.7", (msg.data[2]&0x80)>0 );
+				}
+				
+				if( NodeType == "in" ){
+					UpdateObjectOnChange( DeviceName, "DI4.0", DeviceName+".DI4.0", (msg.data[3]&0x01)>0 );
+					UpdateObjectOnChange( DeviceName, "DI4.1", DeviceName+".DI4.1", (msg.data[3]&0x02)>0 );
+					UpdateObjectOnChange( DeviceName, "DI4.2", DeviceName+".DI4.2", (msg.data[3]&0x04)>0 );
+					UpdateObjectOnChange( DeviceName, "DI4.3", DeviceName+".DI4.3", (msg.data[3]&0x08)>0 );
+					
+					var temp = (msg.data[4] + msg.data[5] * 256) * 0.0625;
+					
+					UpdateObjectOnChange( DeviceName, "T1", DeviceName+".OneWireBus.T1", temp );
+					
+				}
+				
+				
+				
+				/*
+				2018-12-22
 				
 				// Create a object for Device Name
 				adapter.setObjectNotExists(DeviceName, { 
@@ -314,6 +486,8 @@ function main() {
 					
 					SetAndUpdateState( DeviceName+".OneWireBus.T1", "OneWire Tempsensor 1 [°C]", temp.toFixed(2) );
 				}
+				
+				*/
 			}
 			
 			
